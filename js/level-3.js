6d934d1390cc45c702ff859ffa80a11854f52c60
imransarkerweb <imransarker.web@gmail.com>
2023-03-14 12:51:40 +0600

    level-3 solved

---
+++ js/level-3.js
@@ -0,0 +1,54 @@
+const nameField = document.getElementById('name');
+const sendName = document.getElementById('send-name');
+const deleteName = document.getElementById('delete-name');
+
+const emailField = document.getElementById('email')
+const sendEmailBtn = document.getElementById('send-email')
+const deleteEmailBtn = document.getElementById("delete-email")
+
+const messageField = document.getElementById('message')
+const sendMessageBtn = document.getElementById('send-message')
+const deleteMessageBtn = document.getElementById('delete-message')
+const resetBtn = document.getElementById('reset')
+
+//send name code here
+sendName.addEventListener('click', function(){
+    let name = nameField.value;
+    nameField.value = '';
+    localStorage.setItem('name', name);
+})
+
+deleteName.addEventListener('click', function(){
+    localStorage.removeItem('name')
+})
+
+
+
+
+//send email
+
+sendEmailBtn.addEventListener('click', function(){
+    let email = emailField.value;
+    emailField.value = '';
+    localStorage.setItem('email', email);
+})
+
+deleteEmailBtn.addEventListener('click', function(){
+    localStorage.removeItem('email')
+})
+
+//send message code
+sendMessageBtn.addEventListener('click', function(){
+    let message = messageField.value;
+    messageField.value = '';
+    localStorage.setItem('message', message);
+})
+
+deleteMessageBtn.addEventListener('click', function(){
+    localStorage.removeItem('message')
+})
+
+
+resetBtn.addEventListener('click', function(){
+    localStorage.clear()
+})
